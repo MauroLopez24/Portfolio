3875261dc961957aaad127b4c079f2ff4a895032
Mauro Lopez <mauro00lopez99@gmail.com>
2026-01-16 00:34:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,69 @@
+function openMobileMenu() {
+    const menu = document.getElementById('header-links');
+    menu.style.display = 'flex';
+    menu.classList.add('show');
+}
+
+function closeMobileMenu() {
+    const menu = document.getElementById('header-links');
+    menu.style.display = 'none';
+    menu.classList.remove('show');
+}
+
+document.querySelectorAll('.header_links a').forEach(link => {
+    link.addEventListener('click', () => {
+        if (window.innerWidth <= 768) {
+            closeMobileMenu();
+        }
+    });
+});
+
+window.addEventListener('resize', () => {
+    const menu = document.getElementById('header-links');
+    if (window.innerWidth > 768) {
+        menu.style.display = 'flex';
+        menu.classList.remove('show');
+    } else {
+        menu.style.display = 'none';
+    }
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    
+    if (targetElement) {
+      const headerOffset = 90; 
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+      
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+      
+      if (window.innerWidth <= 768) {
+        document.getElementById('header-links').style.display = 'none';
+      }
+    }
+  });
+});
+
+document.querySelectorAll('.section-projects_card').forEach(card => {
+  card.addEventListener('click', function(e) {
+    if (!e.target.closest('a')) {
+      const link = this.querySelector('.section-projects_card-footer a');
+      if (link) {
+        window.open(link.href, '_blank');
+      }
+    }
+  });
+});
+
 document.getElementById('contact-form').addEventListener('submit', async function(e) {
   e.preventDefault();
 
@@ -19,6 +85,9 @@ document.getElementById('contact-form').addEventListener('submit', async functio
       document.getElementById('success-message').style.display = 'block';
       document.getElementById('error-message').style.display = 'none';
       this.reset();
+      
+      
+      
     } else {
       throw new Error('Error en el envío');
     }
@@ -26,5 +95,10 @@ document.getElementById('contact-form').addEventListener('submit', async functio
     console.error('Error:', error);
     document.getElementById('error-message').style.display = 'block';
     document.getElementById('success-message').style.display = 'none';
+    
+    
+    
   }
 });
+
+
